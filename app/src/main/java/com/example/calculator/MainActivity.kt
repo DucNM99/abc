4fb9ef4ae67ecb7dd1d9457776012db275365c8f
DucNM99 <ducmanh.nmd@gmail.com>
2020-10-13 23:42:23 +0700

    UI Calculator

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private EditText tvMath;
+    private TextView tvResult;
+
+    private Button btnPT;
+    private Button btnCE;
+    private Button btnC;
+    private Button btnDEL;
+    private Button btnA;
+    private Button btnB;
+    private Button btnCAN;
+    private Button btnDIV;
+    private Button btn7;
+    private Button btn8;
+    private Button btn9;
+    private Button btnMul;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btnSub;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btnPlus;
+    private Button btnAB;
+    private Button btn0;
+    private Button btnDot;
+    private Button btnResult;
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    public void initWidget(){
+
+    }
+}
